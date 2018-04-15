d2d9eba11ac8db4fef80f490047f76e22569cd27
wangn <3579677768@qq.com>
2018-04-15 19:26:06 +0800

    init

--- CloudConfig/src/main/java/top/itning/cloudconfig/CloudconfigApplication.java
+++ CloudConfig/src/main/java/top/itning/cloudconfig/CloudconfigApplication.java
@@ -2,8 +2,12 @@ package top.itning.cloudconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
 public class CloudconfigApplication {
 
     public static void main(String[] args) {
